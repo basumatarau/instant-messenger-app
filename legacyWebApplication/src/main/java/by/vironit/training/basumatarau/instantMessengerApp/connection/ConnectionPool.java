c25e7af8d1c8344226b5836af6891f0a86e01509
basumatarau <basumatarau@gmail.com>
2019-06-12 13:12:15 +0300

    connection pooling fixed (proxy equals stub)

--- legacyWebApplication/src/main/java/by/vironit/training/basumatarau/instantMessengerApp/connection/ConnectionPool.java
+++ legacyWebApplication/src/main/java/by/vironit/training/basumatarau/instantMessengerApp/connection/ConnectionPool.java
@@ -6,20 +6,18 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class ConnectionPool {
     private static ConnectionPool instance;
 
-    private static final String DB_DRIVER = "datasource.driver";
-    private static final String DB_URL = "datasource.url";
-    private static final String DB_LOGIN = "datasource.username";
-    private static final String DB_PASSWORD = "datasource.password";
-    private static final String DB_POOL_SIZE = "datasource.poolsize";
-
     private BlockingQueue<Connection> connectionQueue;
     private BlockingQueue<Connection> givenAwayConnectionQueue;
+    private Map<Connection, Connection> lookUpDictionary;
+
     private String driverName;
     private String url;
     private String login;
@@ -27,23 +25,12 @@ public class ConnectionPool {
     private int poolSize;
 
     private ConnectionPool() {
-        //todo fix properties loading ... omfg
-        //final ConnectionProperties propManager = ConnectionProperties.getInstance();
-/*
 
-        driverName = propManager.getValue(DB_DRIVER);
-        url = propManager.getValue(DB_URL);
-        login = propManager.getValue(DB_LOGIN);
-        password = propManager.getValue(DB_PASSWORD);
-        poolSize = Integer.parseInt(propManager.getValue(DB_POOL_SIZE));
-*/
-
-        driverName = "org.postgresql.Driver";
-        url = "jdbc:postgresql://localhost:5432/im-legacy-db-training-project";
-        login = "postgres";
-        password = "password";
-        poolSize = 20;
-
+        driverName = DatasourceProperties.DRIVER_NAME;
+        url = DatasourceProperties.URL;
+        login = DatasourceProperties.LOGIN;
+        password = DatasourceProperties.PASSWORD;
+        poolSize = DatasourceProperties.POOLSIZE;
 
         try {
             initPoolData();
@@ -66,6 +53,7 @@ public class ConnectionPool {
     private void initPoolData() throws ConnectionPoolException {
         try {
             Class.forName(driverName);
+            lookUpDictionary = new HashMap<>();
             connectionQueue = new ArrayBlockingQueue<>(poolSize);
             givenAwayConnectionQueue = new ArrayBlockingQueue<>(poolSize);
 
@@ -74,13 +62,13 @@ public class ConnectionPool {
                 final PooledConnectionInvocationHandler<Connection> invocationHandler
                         = new PooledConnectionInvocationHandler<>(con);
 
-                Connection connection
+                Connection connectionProxy
                         = ((PooledConnection) Proxy.newProxyInstance(
                         con.getClass().getClassLoader(),
                         new Class[]{PooledConnection.class},
                         invocationHandler));
-
-                connectionQueue.add(connection);
+                connectionQueue.add(connectionProxy);
+                lookUpDictionary.put(con, connectionProxy);
             }
         } catch (ClassNotFoundException e) {
             throw new ConnectionPoolException("driver not found", e);
@@ -93,6 +81,7 @@ public class ConnectionPool {
 
     public void dispose() {
         clearConnectionQueue();
+        lookUpDictionary.clear();
     }
 
     private void clearConnectionQueue() {
@@ -169,8 +158,8 @@ public class ConnectionPool {
     }
 
     /**
-     * TODO TODO TODO fix the modafaka
-     * doesn't work :((((((((((((((
+     * to be revised...
+     * fixed ¯\_(ツ)_/¯
      */
 
     private interface PooledConnection extends Connection {
@@ -195,16 +184,21 @@ public class ConnectionPool {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             if (method.getName().equals("close") && method.getParameterCount() == 0) {
                 proxiedClose();
-            }
-            if (method.getName().equals("reallyClose") && method.getParameterCount() == 0) {
+                return null;
+            }else if (method.getName().equals("reallyClose") && method.getParameterCount() == 0) {
                 final Method close
                         = invocationTarget.getClass().getDeclaredMethod("close");
                 return close.invoke(invocationTarget);
+            } else if(method.getName().equals("equals") && method.getParameterCount() == 1){
+                return lookUpDictionary.get(invocationTarget) == args[0];
+            }else {
+                return method.invoke(invocationTarget, args);
             }
-            return method.invoke(invocationTarget, args);
         }
 
         private void proxiedClose() throws SQLException {
+            Connection conProxy = lookUpDictionary.get(invocationTarget);
+
             if (invocationTarget.isClosed()) {
                 throw new SQLException(
                         "close() method invocation on closed connection instance");
@@ -212,11 +206,12 @@ public class ConnectionPool {
             if (invocationTarget.isReadOnly()) {
                 invocationTarget.setReadOnly(false);
             }
-            if (!givenAwayConnectionQueue.remove(invocationTarget)) {
+
+            if (!givenAwayConnectionQueue.remove(conProxy)) {
                 throw new SQLException("illegal state exception: \"in use\" connection " +
                         "queue doesn't have the connection instance");
             }
-            if (!connectionQueue.offer(invocationTarget)) {
+            if (!connectionQueue.offer(conProxy)) {
                 throw new SQLException("illegal state exception: \"free\" connection " +
                         "queue already has the connection instance");
             }
