b3a6e252516e714537a6d141fc4340e4a15874d5
basumatarau <basumatarau@gmail.com>
2019-07-10 15:41:35 +0300

    modularization

---
+++ im-secure-resource-server/src/test/java/by/vironit/training/basumatarau/instantMessengerApp/persistenceTests/repositoryTests/MessageResourceRepositoryTest.java
@@ -0,0 +1,13 @@
+package by.vironit.training.basumatarau.instantMessengerApp.persistenceTests.repositoryTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MessageResourceRepositoryTest extends BaseRepositoryTest{
+    @Test
+    public void toBeImplemented(){
+        //todo sending images is to be implemented after everything else is done... I guess...
+        //fail("to be implemented");
+    }
+}
